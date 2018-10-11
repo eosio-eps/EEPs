d23be4585d0f3efd3473384538e052ae739d25e7
joshkauffman <40213896+joshkauffman@users.noreply.github.com>
2018-10-10 22:56:39 -0400

    Update .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e # halt script on error
 
-HTMLPROOFER_OPTIONS="./_site --internal-domains=eips.ethereum.org --check-html --check-opengraph --report-missing-names --log-level=:debug --assume-extension --empty-alt-ignore --timeframe=6w --url-ignore=/EIPS/eip-1,EIPS/eip-1,/EIPS/eip-107,/EIPS/eip-858"
+HTMLPROOFER_OPTIONS="./_site --internal-domains=eose.app --check-html --check-opengraph --report-missing-names --log-level=:debug --assume-extension --empty-alt-ignore --timeframe=6w --url-ignore=/EEPS/eep-1,EEPS/eep-1,/EEPS/eep-107,/EEPS/eep-858"
 
 if [[ $TASK = 'htmlproofer' ]]; then
   bundle exec jekyll doctor
@@ -14,14 +14,14 @@ elif [[ $TASK = 'htmlproofer-external' ]]; then
   bundle exec jekyll doctor
   bundle exec jekyll build
   bundle exec htmlproofer $HTMLPROOFER_OPTIONS --external_only
-elif [[ $TASK = 'eip-validator' ]]; then
-  BAD_FILES="$(ls EIPS | egrep -v "eip-[0-9]+.md|eip-20-token-standard.md")" || true
+elif [[ $TASK = 'eep-validator' ]]; then
+  BAD_FILES="$(ls EEPS | egrep -v "eip-[0-9]+.md|eep-20-token-standard.md")" || true
   if [[ ! -z $BAD_FILES ]]; then
     echo "Files found with invalid names:"
     echo $BAD_FILES
     exit 1
   fi
 
-  FILES="$(ls EIPS/*.md | egrep "eip-[0-9]+.md")"
-  bundle exec eip_validator $FILES
+  FILES="$(ls EEPS/*.md | egrep "eep-[0-9]+.md")"
+  bundle exec eep_validator $FILES
 fi
